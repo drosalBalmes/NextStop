99721966679427d954280333fffd6bfa3c26e90b
aochoa <aochoa21@jaumebalmes.net>
2023-04-27 21:17:59 +0200

    error

---
+++ 2223DAMP3Grup01/app/src/main/java/com/example/a2223damp3grup01/activities/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.a2223damp3grup01.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.a2223damp3grup01.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
